adcb2754c5009e08bad785c5cf8475778a233c17
NinoBonev <nbonev@gmail.com>
2018-06-22 23:39:08 +0100

    Deleted Old Instance

--- JavaFundamentalsSeptember2017/Java Advanced/Exercises/01. INTRO TO JAVA - LAB/06. Numbers 0..9/NumbersZeroNine.java
+++
@@ -1,8 +0,0 @@
-public class NumbersZeroNine {
-    public static void main(String[] args) {
-
-        for (int i = 0; i < 10; i++) {
-            System.out.printf("Number: %d%n", i);;
-        }
-    }
-}
