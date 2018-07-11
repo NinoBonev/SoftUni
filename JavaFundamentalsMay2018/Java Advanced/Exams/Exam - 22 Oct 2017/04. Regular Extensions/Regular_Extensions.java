968177d8834ec5631ee111f28e704251cca0836e
NinoBonev <nbonev@gmail.com>
2018-07-11 17:31:14 +0100

    New Tasks Solved

--- JavaFundamentalsMay2018/Java Advanced/Exams/Exam - 22 Oct 2017/04. Regular Extensions/Regular_Extensions.java
+++ JavaFundamentalsMay2018/Java Advanced/Exams/Exam - 22 Oct 2017/04. Regular Extensions/Regular_Extensions.java
@@ -1,24 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Nino Bonev - 2.7.2018 г., 12:06
  */
 public class Regular_Extensions {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
 
-        Integer x = Integer.parseInt(scanner.nextLine());
-        String y = "";
-        String z = "";
+        String input = scanner.readLine();
+        String line;
 
-        if (x % 2 == 0){
-            y = "yes";
-        } else {
-            y = "no";
+        while (!"Print".equals(line = scanner.readLine())){
+            String[] inputs = line.split("%", -1);
+            for (int i = 0; i < inputs.length; i++) {
+                inputs[i] = Pattern.quote(inputs[i]);
+            }
+
+            Pattern pattern = Pattern.compile(String.join("\\S*", inputs));
+            Matcher matcher = pattern.matcher(input);
+
+            while (matcher.find()){
+                String currentWord = matcher.group();
+                StringBuilder reversed = new StringBuilder(currentWord).reverse();
+                input = input.replace(currentWord, reversed.toString());
+            }
         }
 
-        z = x % 2 == 0 ? "yes" : "no";
-        System.out.println(z);
+        System.out.println(input);
 
     }
 }
